ebc8626e9e76fdfef5b2ec611724afa611dfb7aa
Abiemi-ux <abiemib@gmail.com>
2026-07-17 07:22:57 +0300

    Update CORS_ALLOWED_ORIGINS and CSRF_TRUSTED_ORIGINS to include new domain

--- config/settings.py
+++ config/settings.py
@@ -64,13 +64,14 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "https://simple-survey-api-iow2.onrender.com",
+    "https://simple-survey-web.onrender.com",
     "http://127.0.0.1:8001",
     "http://localhost:8001",
 ]
 CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = [
     "https://simple-survey-api-iow2.onrender.com",
-    "https://simple-survey-api-iow2.onrender.com",
+    "https://simple-survey-web.onrender.com",
     "http://127.0.0.1:8001",
     "http://localhost:8001",
 ]
