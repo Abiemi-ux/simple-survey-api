6a6195ded1caa3d962ac24cf5ca69af647567276
Abiemi-ux <abiemib@gmail.com>
2026-07-17 06:55:15 +0300

    Update DEBUG setting to default to True in settings.py

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b3!&4*6@pt@0el!m-#e&4ne^3#*w-^)3jt47#rpi&0cy#u@rwj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 
 ALLOWED_HOSTS = []
